0783b503e616d262e64081009642bcf8f0ba025e
dineshkrishnanandam <dinesh.krishnanandam@gmail.com>
2021-11-01 19:19:41 +0530

    refactoring
    refactoring

---
+++ src/test/java/com/dk/address/book/DkOnlineAddressBookApplicationTest.java
@@ -0,0 +1,19 @@
+//package com.dk.address.book;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+//import org.springframework.util.Assert;
+//
+//import com.dk.address.book.contact.ContactController;
+//
+//@WebMvcTest(controllers = ContactController.class)
+//public class DkOnlineAddressBookApplicationTest {
+//
+//	
+//
+//	@Test
+//	public void homePage() throws Exception {
+//		Assert.isTrue(false, "");
+//	}
+//	
+//}
